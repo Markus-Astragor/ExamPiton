9a9f13bb26d16dadad6255e8bb9a5b71dabe5a3d
Marko Kosyk <kosyk.markian@gmail.com>
2023-12-14 10:48:38 +0200

    [Changes] new changes crud system added

--- Exam.py
+++ Exam.py
@@ -3,12 +3,14 @@
 # Розробіть систему організації завдань, 
 # де кожне завдання має певний пріоритет. Використайте декоратори для присвоєння пріоритетів завданням. 
 # Створіть функції для виведення списку завдань, впорядкованих за пріоритетом, та виконання завдань відповідно до пріоритету.
-
+import uuid
+import re
 
 class Task:
-    def __init__(self, name, priority=0):
+    def __init__(self, name, priority=0, id=0):
         self.name = name
         self.priority = priority
+        self.id = id
 
 def prioritize(func):
     def wrapper(*args, **kwargs):
@@ -31,8 +33,8 @@ class TaskManager(metaclass=TaskManagerMeta):
         self.tasks = []
 
     @prioritize
-    def create_task(self, name, priority):
-        return Task(name, priority)
+    def create_task(self, name, priority, id):
+        return Task(name, priority, id)
 
     def add_task(self, task):
         self.tasks.append(task)
@@ -40,16 +42,66 @@ class TaskManager(metaclass=TaskManagerMeta):
     def get_sorted_tasks(self):
         return sorted(self.tasks, key=lambda x: x.priority)
     
-    def print_tasks(self, tasks):
-        for task in tasks:
-            print(f"Завдання: {task.name}, Пріоритет: {task.priority}")
+    def print_tasks(self):
+        for task in self.tasks:
+            print(f"Task: {task.name}, Priority: {task.priority}, id:{task.id}")
+
+    def find_task_by_id(self, id):
+        success = False
+        for task in self.tasks:
+            if re.sub(r'urn:uuid:', '', task.id.urn) == id:
+                success = True
+                return task
+        if not success: return 'Not task with such id'
+    
+    def delete_task_by_id(self, id):
+        for i,task in enumerate(self.tasks):
+            if re.sub(r'urn:uuid:', '', task.id.urn) == id:
+                del self.tasks[i]
 
 
-task_manager = TaskManager()
+while True:
+    task_manager = TaskManager()
+    print('Hello user. Enter your answer:\n1)Create a task\n2)Read tasks\n3)Update a task\n4)Delete a task\n5)Exit')
+    answer = int(input('Enter your answer\n'))
+    match answer:
+        case 1:
+            task_text = input('Enter a text for your task\n')
+            prioritization_num = int(input('Enter a number to prioritize task\n'))
+            task_manager.add_task(task_manager.create_task(task_text, prioritization_num, uuid.uuid4()))
+        case 2:
+            sorted_tasks = task_manager.get_sorted_tasks()
+            task_manager.print_tasks()
+        case 3:
+            answer_id = input('Enter the id of task you want to edit\n')
+            needed_task = task_manager.find_task_by_id(answer_id)
+            print(needed_task)
+            if needed_task != 'Not task with such id': 
+                print('What do you want to change?\n1)Name\n2)Priority')
+                answer = int(input('Enter your answer\n'))
+                match answer:
+                    case 1:
+                        enter_name_for_task = input('Enter text for task')
+                        needed_task.name = enter_name_for_task
+                        task_manager.print_tasks()
+                    case 2:
+                        enter_priority = int(input('Enter a number for prioritization\n'))
+                        needed_task.priority = enter_priority
+                        task_manager.print_tasks()
+                    case _:
+                        print('Enter sth normal')
+        
+        case 4:
+            answer_id = input('Enter the id of task you want to edit\n')
+            needed_task = task_manager.find_task_by_id(answer_id)
+            print(needed_task)
+            if needed_task != 'Not task with such id':
+                task_manager.delete_task_by_id(answer_id)
 
-task_manager.add_task(task_manager.create_task("Завдання 3", 3))
-task_manager.add_task(task_manager.create_task("Завдання 1", 1))
-task_manager.add_task(task_manager.create_task("Завдання 2", 2))
+        case 5:
+            quit()
+        case _:
+            print('Enter sth normal')
 
-sorted_tasks = task_manager.get_sorted_tasks()
-task_manager.print_tasks(sorted_tasks)
+
+
